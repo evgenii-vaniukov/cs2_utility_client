44474fad21af0e760d9e7f570643c2b927c3bc6e
Evgenii Vaniukov <evgeniivaniukov64@gmail.com>
2023-08-06 22:06:45 +0300

    Deletes console.log().

--- src/app/features/filter_grenades/filter_grenades.jsx
+++ src/app/features/filter_grenades/filter_grenades.jsx
@@ -97,7 +97,6 @@ export default function GrenadesFilter() {
     );
 
     if (compostiteFilter) {
-      console.log(compostiteFilter);
       setCompositeFilter(compostiteFilter);
     }
   }, []);
