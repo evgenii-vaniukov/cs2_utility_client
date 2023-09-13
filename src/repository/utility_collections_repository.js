cbacb90393551753d292bca779388571e8db7fb7
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-09-13 20:58:51 +0300

    Deletes comments.

--- src/repository/utility_collections_repository.js
+++ src/repository/utility_collections_repository.js
@@ -1,18 +1,5 @@
 import axios from "axios";
 
-const apiUrl = process.env.BASE_URL;
-
-// console.log(baseUrl);
-
-// const getUtilityCollections = async () => {
-//   try {
-//     const response = await fetch("http//localhost:3000/utilitycollections");
-//     return response.data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
 const api = axios.create({
   baseURL: "http://localhost:3000",
   headers: {
