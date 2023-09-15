e3995887eccfeb7ecfc2c6f1b11e518c52d06de0
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-09-15 16:09:48 +0300

    Implements Image component.

--- src/features/collection_details/components/card.jsx
+++ src/features/collection_details/components/card.jsx
@@ -1,11 +1,13 @@
+import Image from "next/image";
 export default function Card({ utility }) {
   return (
     <div>
-      <div className="aspect-h-2 aspect-w-3 w-full overflow-hidden rounded-lg">
-        <img
+      <div className="aspect-h-2 aspect-w-3  w-full overflow-hidden rounded-lg">
+        <Image
           src={utility.landing}
           alt="Drawstring top with elastic loop closure and textured interior padding."
-          className="h-full w-full object-cover object-center"
+          fill
+          layout="cover"
         />
       </div>
       <p className="mt-8 text-base text-gray-500">{utility.label}</p>
