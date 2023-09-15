0a616e4c8cb33c029823e61fb399433e82fcdc30
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-09-15 16:16:03 +0300

    Replaces footer.

--- src/features/filter_collections/dashboard.jsx
+++ src/features/filter_collections/dashboard.jsx
@@ -1,7 +1,7 @@
 "use client";
+import Footer from "../footer";
 import CollectionsList from "./components/collections_list";
 import Filters from "./components/filters";
-import Footer from "./components/footer";
 
 export default function Dashboard({ utilityCollections }) {
   return (
