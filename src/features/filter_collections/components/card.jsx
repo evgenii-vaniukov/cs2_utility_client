de871cabc3997afbce1151c75f7566978106be7f
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-09-22 23:00:59 +0300

    Comments a collection thumbnail.

--- src/features/filter_collections/components/card.jsx
+++ src/features/filter_collections/components/card.jsx
@@ -1,18 +1,17 @@
-import Image from "next/image";
 export default function Card({ utilityCollection, onClick }) {
   return (
     <article
       onClick={onClick}
       className="flex flex-col items-start justify-between rounded-xl p-3 shadow-md"
     >
-      <div className="relative aspect-[16/9] w-full rounded-2xl sm:aspect-[2/1] lg:aspect-[3/2]">
+      {/* <div className="relative aspect-[16/9] w-full rounded-2xl sm:aspect-[2/1] lg:aspect-[3/2]">
         <Image
           src={utilityCollection.thumbnail}
           alt=""
           fill={true}
           className=" rounded-xl"
         />
-      </div>
+      </div> */}
       <div className="max-w-xl">
         <div className="mt-8 flex items-center gap-x-4 text-xs">
           <p
