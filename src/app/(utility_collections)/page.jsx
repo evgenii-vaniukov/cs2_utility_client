cb728d8216180e194c5f2af68d228fcefdb87a26
evgenii-vaniukov <evgeniivaniukov64@gmail.com>
2023-10-01 16:06:22 +0300

    Revert "."
    This reverts commit 1b80f8782be901e80925b2856434696616a6db1c.

--- src/app/(utility_collections)/page.jsx
+++ src/app/(utility_collections)/page.jsx
@@ -1,7 +1,8 @@
 import Dashboard from "@/features/filter_collections/dashboard";
 
 import { getLikesCount } from "@/repository/analytics/likes/likes_repository";
-import { getUtilityCollections } from "@/repository/utility_collections/get_utility_collections";
+import { getUtilityCollections } from "@/repository/utility_collections/utility_collections_repository";
+export const revalidate = 60;
 
 async function UtilityCollections() {
   const utilityCollections = await getUtilityCollections();
